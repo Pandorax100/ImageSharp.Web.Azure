1ec40a70421a97c7b30ae940195d21ca27b5ef2f
Liam McDermott <44736094+Pandorax100@users.noreply.github.com>
2019-05-03 13:47:40 +0100

    Add Configure Blob Cache Extension Mehtods

--- src/ImageSharp.Web.Azure/DependencyInjection/ServiceConfigurationExtensions.cs
+++ src/ImageSharp.Web.Azure/DependencyInjection/ServiceConfigurationExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Pandorax.ImageSharp.Web.Azure.Caching;
 using Pandorax.ImageSharp.Web.Azure.Providers;
 
 namespace Pandorax.ImageSharp.Web.Azure.DependencyInjection
@@ -36,6 +37,36 @@ namespace Pandorax.ImageSharp.Web.Azure.DependencyInjection
         public static IServiceCollection ConfigureBlobProviderOptions(this IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<BlobStorageImageProviderOptions>(configuration);
+
+            return services;
+        }
+
+        /// <summary>
+        /// Registers an action used to configure <see cref="BlobCacheOptions"/>.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
+        /// <param name="configureOptions">The action used to configure the options.</param>
+        /// <returns>
+        /// The <see cref="IServiceCollection"/> so that additional calls can be chained.
+        /// </returns>
+        public static IServiceCollection ConfigureBlobCacheOptions(this IServiceCollection services, Action<BlobCacheOptions> configureOptions)
+        {
+            services.Configure(configureOptions);
+
+            return services;
+        }
+
+        /// <summary>
+        /// Registeres a configuration instance which <see cref="BlobCacheOptions"/> will bind to.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
+        /// <param name="configuration">The configuration being bound.</param>
+        /// <returns>
+        /// The <see cref="IServiceCollection"/> so that additional calls can be chained.
+        /// </returns>
+        public static IServiceCollection ConfigureBlobCacheOptions(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<BlobCacheOptions>(configuration);
 
             return services;
         }
